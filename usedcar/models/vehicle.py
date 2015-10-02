d03f981d1bb8d307a70e44bf94c9bc4134002d15
xiejueheng <xiejueheng@126.com>
2015-10-02 15:24:43 +0800

    commit

--- usedcar/models/vehicle.py
+++ usedcar/models/vehicle.py
@@ -6,6 +6,9 @@ from ._base import db, SessionMixin
 
 __all__ = ('VehicleType', 'VehicleStyle', 'VehicleInfo', 'VehicleImg')
 
+"""
+车型VehicleType
+"""
 class VehicleType(db.Model, SessionMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(40), unique=True, index=True,
@@ -30,6 +33,9 @@ class VehicleType(db.Model, SessionMixin):
 	def __repr__(self):
 		return '<VehicleType: %s>' % self.name
 
+"""
+车款VehicleStyle
+"""
 class VehicleStyle(db.Model, SessionMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(40), unique=True, index=True,
@@ -43,3 +49,45 @@ class VehicleStyle(db.Model, SessionMixin):
 	gear = db.Column(db.Integer, default=0)
 	gate = db.Column(db.Integer, default=0)
 	produce_year = db.Column(db.Integer, default=0)
+
+	def __str__(self):
+		return self.name
+
+	def __repr__(self):
+		return '<VehicleStyle: %s>' % self.name
+
+"""
+车辆信息VehicleInfo
+"""
+class VehicleInfo(db.Model, SessionMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	vehicle_style =  db.Column(db.Integer, default=0)
+	mileage = db.Column(db.Integer, default=0)
+	vehicle_age = db.Column(db.Integer, default=0)
+	register_time = db.Column(db.DateTime, default=datetime.utcnow)
+	source_type = db.Column(db.Integer, default=0)
+	vehicle_typeid = db.Column(db.Integer, default=0)
+	brand_id = db.Column(db.Integer, default=0)
+
+	def __str__(self):
+		return self.id
+
+	def __repr__(self):
+		return '<VehicleInfo: %s>' % self.id
+
+
+"""
+车辆图片
+"""
+class VehiclePhoto(db.Model, SessionMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	vehicle_id = db.Column(db.Integer, default=0)
+	photo_type = db.Column(db.Integer, default=0)
+	photo_url = db.Column(db.String(200), unique=True, index=True,
+						 nullable=False)
+
+	def __str__(self):
+		return '%s %s' %(self.id, self.vehicle_id)
+
+	def __repr__(self):
+		return '<VehiclePhoto: %s_%s>' % (self.id,self.vehicle_id)
