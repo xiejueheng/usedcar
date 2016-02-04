574a2ca72ec54c23a91259b0a4c7ec1968ae1ebf
xiejueheng <xiejueheng@126.com>
2016-02-04 23:56:43 +0800

    update sales.py

--- usedcar/forms/sales.py
+++ usedcar/forms/sales.py
@@ -162,4 +162,174 @@ class AppraiseForm(BaseForm):
 		return salesVehicle
 
 class IndividualForm(BaseForm):
-	pass
+	"""品牌ID"""
+	brandId = IntegerField(
+		'brandId', validators=[DataRequired()],
+		description=_('brandId required')
+	)
+
+	""" 车系ID"""
+	typeId = IntegerField(
+		'typeId', validators=[DataRequired()],
+		description=_('typeId required')
+	)
+
+	""" 车款ID"""
+	styleId = IntegerField(
+		'styleId', validators=[DataRequired()],
+		description=_('styleId required')
+	)
+
+	""" 车身颜色"""
+	color = IntegerField(
+		'color', validators=[DataRequired()],
+		description=_('color required')
+	)
+
+	""" 上牌时间，年份 """
+	licesingYear = IntegerField(
+		'licesingYear', validators=[DataRequired()],
+		description=_('licesingYear required')
+	)
+
+	""" 里程 """
+	mileage = IntegerField(
+		'mileage', validators=[DataRequired()],
+		description=_('mileage required')
+	)
+
+	""" 裸车价格"""
+	price = IntegerField(
+		'price', validators=[DataRequired()],
+		description=_('price required')
+	)
+
+	def save(self):
+		salesVehicle = SalesVehicle(**self.data)
+		salesVehicle.sales_type = 3
+		salesVehicle.save()
+		return salesVehicle
+
+"""出售车辆（置换卖）"""
+class ReplaceForm(BaseForm):
+	""" 品牌ID """
+	brandId = IntegerField(
+		'brandId', validators=[DataRequired()],
+		description=_('brandId required')
+	)
+
+	"""车系ID"""
+	typeId = IntegerField(
+		'typeId', validators=[DataRequired()],
+		description=_('typeId required')
+	)
+
+	"""车款ID"""
+	styleId = IntegerField(
+		'styleId', validators=[DataRequired()],
+		description=_('styleId required')
+	)
+
+	"""上牌时间"""
+	licesingYear = IntegerField(
+		'licesingYear', validators=[DataRequired()],
+		description=_('licesingYear required')
+	)
+
+	""" 里程 """
+	mileage = IntegerField(
+		'mileage', validators=[DataRequired()],
+		description=_('mileage required')
+	)
+
+	"""卖家姓名"""
+	name = StringField(
+		'name', validators=[DataRequired()],
+		description=_('name required')
+	)
+
+	""" 手机号码"""
+	mobile = StringField(
+		_('mobile'), validators=[DataRequired()],
+		description=_('mobile required')
+	)
+
+	""" 验证码"""
+	verifyCode = StringField(
+		_('verifyCode'), validators=[DataRequired()],
+		description=_('verifyCode required')
+	)
+
+	""" 品牌ID """
+	replaceBrandId = IntegerField(
+		'brandId', validators=[DataRequired()],
+		description=_('brandId required')
+	)
+
+	"""车系ID"""
+	replaceTypeId = IntegerField(
+		'typeId', validators=[DataRequired()],
+		description=_('typeId required')
+	)
+
+	"""车款ID"""
+	replaceStyleId = IntegerField(
+		'styleId', validators=[DataRequired()],
+		description=_('styleId required')
+	)
+
+	"""经销商ID"""
+	agencyId = IntegerField(
+		'agencyId', validators=[DataRequired()],
+		description=_('agencyId required')
+	)
+
+""" 6.4.出售车辆（经销商代理） """
+class AgencyForm(BaseForm):
+	""" 品牌ID """
+	brandId = IntegerField(
+		'brandId', validators=[DataRequired()],
+		description=_('brandId required')
+	)
+
+	"""车系ID"""
+	typeId = IntegerField(
+		'typeId', validators=[DataRequired()],
+		description=_('typeId required')
+	)
+
+	"""车款ID"""
+	styleId = IntegerField(
+		'styleId', validators=[DataRequired()],
+		description=_('styleId required')
+	)
+
+	"""卖家姓名"""
+	name = StringField(
+		'name', validators=[DataRequired()],
+		description=_('name required')
+	)
+
+	""" 手机号码"""
+	mobile = StringField(
+		_('mobile'), validators=[DataRequired()],
+		description=_('mobile required')
+	)
+
+	"""上牌时间"""
+	licesingYear = IntegerField(
+		'licesingYear', validators=[DataRequired()],
+		description=_('licesingYear required')
+	)
+
+	""" 验证码"""
+	verifyCode = StringField(
+		_('verifyCode'), validators=[DataRequired()],
+		description=_('verifyCode required')
+	)
+
+	def save(self):
+		salesVehicle = SalesVehicle(**self.data)
+		salesVehicle.sales_type = 5
+		salesVehicle.save()
+		return salesVehicle
